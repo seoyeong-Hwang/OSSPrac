fe7c81154a4a5255dd8493636854bd2c737131da
seoyeong-Hwang <2022110463@dgu.ac.kr>
2024-04-05 10:17:51 +0900

    university.py

--- Psub5/main.py
+++ Psub5/main.py
@@ -7,9 +7,13 @@ num = int(input())
 print("학과를 입력하세요: ", end='')
 depart = input()
 
+print("학교를 입력하세요: ", end='')
+university = input()
+
 print()
 
 print("<출력>")
 print("이름: " + name)
 print("학번: " + str(num))
 print("학과: " + depart)
+print("학교: " + university)
